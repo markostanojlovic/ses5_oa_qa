7a34dfa42b8135313691beb2dad9b06f3072bfd7
Marko Stanojlovic <stanojlovic.marko@gmail.com>
2018-08-09 15:35:11 +0200

    copy_report_to_maia.sh

---
+++ copy_report_to_maia.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Requirement:
+# 	- /home/mstan/.ssh/id_rsa.pub copied to authorized_keys @maia server
+# 	- 
+
+[[ -z $1 ]] && MILESTONE=M0 || MILESTONE=$1
+REPORT=oA_pytest_report_${MILESTONE}.html
+
+# Rename the copy and edit the report 
+cp pytest_report.html $REPORT
+sed -i "s/<h1>pytest_report.html/<h1>SES 5.5 ${MILESTONE} - Openattic/" $REPORT
+
+# Copy the report 
+scp $REPORT root@10.100.96.56:/usr/share/nginx/html/
